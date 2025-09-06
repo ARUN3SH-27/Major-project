9a72ab32c0b5e265f532a5c8f0fcc98b9e45d739
Arunesh V <103491088+ARUN3SH-27@users.noreply.github.com>
2025-09-06 17:13:47 +0530

    Update test.py

--- Dynamic/test.py
+++ Dynamic/test.py
@@ -1,8 +1,9 @@
 import requests
 
-BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAACg7zAEAAAAABBikdwYlorE2FeNpNqrkT8uV1fk%3DsxxU1YZAYwO0G56tjTPSElgal0CEy0HF3zJ4a5jtpmTdRyO4h7"
+BEARER_TOKEN = "twitter token here7"
 API_ENDPOINT = "https://api.twitter.com/2/users/by/username/elonmusk"
 headers = {"Authorization": f"Bearer {BEARER_TOKEN}"}
 params = {"user.fields": "public_metrics,created_at,description,verified"}
 response = requests.get(API_ENDPOINT, headers=headers, params=params)
 print(response.json())
+
